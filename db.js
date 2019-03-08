ed185c7680304950d17bd978c823c32b0a112215
nastromo <nastromo@Sulako.local>
2019-03-08 17:32:49 +0200

    db host added

--- db.js
+++ db.js
@@ -7,6 +7,7 @@ const db = new Sequelize({
     database: process.env.DB_NAME,
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
+    host: process.env.DB_HOST,
     dialect: 'mysql',
     logging: false,
     dialectOptions: {
